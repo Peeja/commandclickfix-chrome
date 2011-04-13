afc06385b06a88cf81c121ac55cea3643576e26e
Peter Jaros <peter.a.jaros@gmail.com>
2011-04-12 22:15:26 -0400

    Fix for Alt key too.

--- ext/commandclickfix.js
+++ ext/commandclickfix.js
@@ -1,7 +1,6 @@
 jQuery.noConflict(true)(function($) {
   document.addEventListener("click", function(event) {
-    console.log("Captured event:", event);
-    if ($(event.target).parents().andSelf().is("a") && event.metaKey) {
+    if ($(event.target).parents().andSelf().is("a") && (event.metaKey || event.altKey)) {
       event.stopPropagation();
     }
   }, true);
